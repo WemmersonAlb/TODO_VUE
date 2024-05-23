49c9c6dc0b2633a55c733059bd2dbd5d91b41b10
WemmersonAlb <wemmerson.albuquerque@gmail.com>
2024-05-23 15:05:09 -0300

    [feat] Delete, primeira parte do edit e persistência do checkado
    Delete: Função adicionada assim que clica no icone da lixeira;
    Primeira parte do edit: Ao clicar no icone da caneta o checkbox é substituido por um input text
    Persistância do checkado: Ao marcar um item dispara uma função que já envia para a api a atualização

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,65 +1,117 @@
-<script>
-export default {
-  data(){
-    return{
-      check1: true,
-      label1: 'teste',
-      riscadoOuNao: 'riscado'
-    }
-  },
-  methods: {
-    mudarRisco(){
-      this.riscadoOuNao = this.riscadoOuNao == 'riscado' ? 'nao' : 'riscado';
-    },
-    teste(a){alert(a?'editar':'deletar')}
-  },
-}
-</script>
 
 <template>
   <div id="arrayToDo">
-    <v-card max-width="300px" class="cardsPadrao">
+    <v-card max-width="300px" class="cardsPadrao" v-for="list in data" :key="list.id">
       <v-template>
         <v-card-item>
-          <h1 class="titleCard">Lista de Compras</h1>
+          <h1 class="titleCard">{{list.title}}</h1>
         </v-card-item>
         <v-card-item>
-          <div class="item">
-            <v-checkbox v-model="check1"  @change="mudarRisco" hide-details >
+          <div class="item" v-for="item in list.allItems" :key="item.id">
+            <v-checkbox  @change="mudarRisco(list.id, item)" hide-details v-model="item.state" v-show="!item.edit">
               <template v-slot:label>
-                <span :class="[riscadoOuNao, 'labelPadrao']">{{ label1 }}</span>
+                <span :class="[item.state? 'riscado': '', 'labelPadrao']">{{ item.text }}</span>
               </template>
             </v-checkbox>
-            <v-icon icon="mdi-pencil" color="grey-darken-4" @click="teste(true)"></v-icon>
-            <v-icon icon="mdi-delete" color="grey-darken-4" @click="teste(false)"></v-icon>
+            <v-text-field v-model="item.text" v-show="item.edit"></v-text-field>
+            <v-icon icon="mdi-pencil" color="grey-darken-4" @click="editItem(list.id, item)"></v-icon>
+            <v-icon icon="mdi-delete" color="grey-darken-4" @click="deleteItem(list.id, item)"></v-icon>
           </div>
-          <div class="item">
-            <v-checkbox v-model="check1"  @change="mudarRisco" hide-details>
-              <template v-slot:label>
-                <span :class="[riscadoOuNao, 'labelPadrao']">{{ label1 }}</span>
-              </template>
-            </v-checkbox>
-          </div>
-          <div class="item">
-            <v-checkbox v-model="check1"  @change="mudarRisco" hide-details>
-              <template v-slot:label>
-                <span :class="[riscadoOuNao, 'labelPadrao']">{{ label1 }}</span>
-              </template>
-            </v-checkbox>
-          </div>
-          <div class="item">
-            <v-checkbox v-model="check1"  @change="mudarRisco" hide-details>
-              <template v-slot:label>
-                <span :class="[riscadoOuNao, 'labelPadrao']">{{ label1 }}</span>
-              </template>
-            </v-checkbox>
-          </div>
-
         </v-card-item>
       </v-template>
     </v-card>
   </div>
 </template>
+<script>
+export default {
+  data(){
+    return{
+      check1: true,
+      riscadoOuNao: 'riscado',
+      data: null,
+      items:[]
+    }
+  },
+  methods: {
+    editItem(idList, item){
+      this.data.forEach(el=>{
+        if(el.id == idList){
+          el.allItems.forEach(it=>{
+            if(it.id == item.id){
+              it.edit = true;
+            }
+          })
+        }
+      });      
+    },
+    async deleteItem(idList, item){
+
+      this.data.forEach(el=>{
+        if(el.id == idList){
+          let index = el.allItems.findIndex(obj=>obj.id == item.id);
+          el.allItems.splice(index, 1);
+        }
+      });
+      const listToUpdate = this.data.find(list=>list.id === idList);
+      const dataJson = JSON.stringify({allItems: listToUpdate.allItems});
+      const req = await fetch(`https://makeyourburger-xxqo.onrender.com/allTodoList/${idList}`,{
+        method: "PATCH",
+        headers: { "Content-Type" : "application/Json" },
+        body: dataJson
+      })
+      const res = await req.json();
+      console.log(res);
+      this.getAllLists();
+
+    },
+    async mudarRisco(idList, item){
+      item.state = !item.state;
+      
+      this.data.forEach(el => {
+        if(el.id == idList){
+          el.allItems.forEach(it =>{
+            if(it.id == item.id){
+              it.state = !it.state;
+            }
+          });
+        }        
+      });
+
+      const listToUpdate = this.data.find(list=>list.id === idList);
+      
+      const dataJson = JSON.stringify({allItems: listToUpdate.allItems});
+      const req = await fetch(`https://makeyourburger-xxqo.onrender.com/allTodoList/${idList}`,{
+        method: "PATCH",
+        headers: { "Content-Type" : "application/Json" },
+        body: dataJson
+      })
+      const res = await req.json();
+      console.log(res);
+      this.getAllLists();
+    },
+
+    teste(a){alert(a?'editar':'deletar')},
+
+    async getAllLists(){
+      let req = await fetch("https://makeyourburger-xxqo.onrender.com/allTodoList");
+      let data = await req.json();
+
+      data.forEach(el=>{
+        el.allItems.forEach(it=>{
+          it.edit = false;
+        })
+      });
+
+      this.data = data;
+      console.log(data)
+    }
+  },
+  mounted(){
+    this.getAllLists();
+  }
+}
+</script>
+
 <style scoped>
   #arrayToDo{
     max-width: 80%;
@@ -95,5 +147,8 @@ export default {
   }
   .v-checkbox{
     margin-right: 10px;
+  }
+  .displayNone{
+    display: none;
   }
 </style>
